f8ce2abb345eea0a56d6720d8ed34d0ba6c18861
Mohamed Gonem <nightangelyt01@gmail.com>
2025-12-28 12:05:59 +0200

    Added Client logic

--- ClientCLI/Program.cs
+++ ClientCLI/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        try
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync("http://localhost:5050/great");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<ApiResponse>(json);
+
+            Console.WriteLine(result?.message);
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    public class ApiResponse
+    {
+        public string message { get; set; } = string.Empty;
+    }
+}
