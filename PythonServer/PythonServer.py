a316eb9a556daee64057dd4c2d52918415058b02
Mohamed Gonem <nightangelyt01@gmail.com>
2025-12-28 11:29:49 +0200

    Added PythonServer to solution as project

---
+++ PythonServer/PythonServer.py
@@ -0,0 +1,10 @@
+from flask import Flask , jsonify
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route("/greet",methods=["GET"])
+def great():
+    return jsonify({"message": "hi from python server"})
+if __name__ == '__main__':
+    serve(app , host='127.0.0.1', port=5050)
